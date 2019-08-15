9d89a82d9d85315619e6ac9f4fa9d901063c9c77
Иван Винокуров <vinokurov@corso.agency>
2019-08-15 13:43:23 +0300

    Routing

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -7,48 +7,15 @@ import barba from '@barba/core';
 import barbaRouter from '@barba/router';
 import barbaCss from '@barba/css';
 import './polyfill';
+import * as StackBlur from 'stackblur-canvas';
 
 svg4everybody();
 
 window.$ = $;
 window.jQuery = $;
-
-const routes = [{
-	path: '/index',
-	name: 'home'
-}, {
-	path: '/about',
-	name: 'about'
-}, {
-	path: '/technologies',
-	name: 'techall'
-}, {
-	path: '/technologies/:technology',
-	name: 'tech'
-}, {
-	path: '/news/:post',
-	name: 'post'
-}, {
-	path: '/news/:event',
-	name: 'event'
-}, {
-	path: '/team',
-	name: 'team'
-}, {
-	path: '/vacancies',
-	name: 'job'
-}];
-
-barba.use(barbaRouter, {
-	routes,
-});
-
-barba.use(barbaCss);
-
-barba.hooks.leave((data) => {
-	$('html,body').animate({ scrollTop: 0 }, 300);
-});
-
-barba.init();
+window.StackBlur = StackBlur;
+window.barba = barba;
+window.barbaRouter = barbaRouter;
+window.barbaCss = barbaCss;
 
 require('ninelines-ua-parser');
