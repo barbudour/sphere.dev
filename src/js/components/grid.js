8b5962ac483a1f273da5dbd4b145ba9905c13851
Tigran <mistique888@gmail.com>
2019-09-05 17:09:50 +0500

    fixes

--- src/js/components/grid.js
+++ src/js/components/grid.js
@@ -8,20 +8,20 @@ function gridItemsBlur() {
 	}
 
 	$gridItem.each((index, element) => {
-		let myCanvas = $(element).find('.grid-item__blur').get(0);
 		let $blurImg = $(element).find('.grid-item__bg img');
 
 		if ($blurImg.length > 0) {
-			myCanvas.width = $blurImg.width() + 20;
-			myCanvas.height = $blurImg.height() + 20;
-
-			let ctx = myCanvas.getContext('2d');
 			let pic = new Image();
 
 			pic.src = $blurImg.attr('src');
 			pic.onload = () => {
-				ctx.filter = 'blur(10px)';
-				ctx.drawImage(pic, 0, 0, $blurImg.width() + 20, $blurImg.height() + 20);
+				$(element).append(`
+					<div class="blur-image"></div>
+				`);
+
+				let $blurContainer = $(element).find('.blur-image');
+
+				$blurContainer.append(pic);
 			};
 		}
 	});
