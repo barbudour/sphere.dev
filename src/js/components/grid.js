37a7e310698fa82af140622e5cb7abc34feaf4e9
Tigran <mistique888@gmail.com>
2019-09-05 18:06:36 +0500

    fixes

--- src/js/components/grid.js
+++ src/js/components/grid.js
@@ -9,30 +9,9 @@ function gridItemsBlur() {
 	}
 
 	$gridItem.each((index, element) => {
-		let myCanvas = $(element).find('.grid-item__blur').get(0);
 		let $blurImg = $(element).find('.grid-item__bg img');
 
 		if ($blurImg.length > 0) {
-			// myCanvas.width = $blurImg.width() + 20;
-			// myCanvas.height = $blurImg.height() + 20;
-
-			// let ctx = myCanvas.getContext('2d');
-			// let pic = new Image();
-
-			// pic.src = $blurImg.attr('src');
-			// pic.onload = () => {
-			// 	ctx.filter = 'blur(10px)';
-			// 	ctx.drawImage(pic, 0, 0, $blurImg.width() + 20, $blurImg.height() + 20);
-
-			// 	$(element).append(`
-			// 		<div class="blur-image"></div>
-			// 	`);
-
-			// 	let $blurContainer = $(element).find('.blur-image');
-
-			// 	$blurContainer.append(pic);
-			// };
-
 			$blurImg
 				.after('<canvas></canvas>');
 
@@ -45,13 +24,10 @@ function gridItemsBlur() {
 				width: `${$blurImg.width()}px`,
 			});
 
-			$canvas.get(0).height = $blurImg.height();
-			$canvas.get(0).width = $blurImg.width();
+			canvas.height = $blurImg.height();
+			canvas.width = $blurImg.width();
 
 			StackBlur.image($blurImg.get(0), $canvas.get(0), 10);
-
-			// canvas.height = $blurImg.height();
-			// canvas.width = $blurImg.width();
 
 			$canvas.css({
 				height: `${$blurImg.height()}px`,
